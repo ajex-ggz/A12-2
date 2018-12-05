599510773fcbeb173f3ba70de9f7b5e5b1af353c
WYJ <1135362890@qq.com>
2018-12-05 08:43:44 +0800

    2

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Mapper
+@Controller
 public class HelloController {
     @RequestMapping("/helloworld")
     public String hello(){
---
+++ src/main/java/com/example/demo/controller/TurnController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TurnController {
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "index";
+    }@RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "login";
+    }@RequestMapping(value = "/signup",method = RequestMethod.GET)
+    public String signup(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "signup";
+    }
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
+    public String about(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "about";
+    }
+    @RequestMapping(value = "/services",method = RequestMethod.GET)
+    public String services(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "services";
+    }
+    @RequestMapping(value = "/portfolio",method = RequestMethod.GET)
+    public String portfolio(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "portfolio";
+    }
+    @RequestMapping(value = "/team",method = RequestMethod.GET)
+    public String team(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "team";
+    }
+    @RequestMapping(value = "/blog",method = RequestMethod.GET)
+    public String blog(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "blog";
+    }
+    @RequestMapping(value = "/contact",method = RequestMethod.GET)
+    public String contact(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        return "contact";
+    }
+}
